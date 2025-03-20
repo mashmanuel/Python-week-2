86c4d971c54494f08b632a442490b08f84d30361
mashmanuel <emmanuelimani98@gmail.com>
2025-03-20 14:54:45 +0300

    Week 2 Python

---
+++ Week 2.py
@@ -0,0 +1,23 @@
+# 1. Create an empty list called my_list
+my_list = []
+
+# 2. Append elements 10, 20, 30, 40
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+# 3. Insert 15 at the second position (index 1)
+my_list.insert(1, 15)
+
+# 4. Extend with [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# 5. Remove last element
+my_list.pop()
+
+# 6. Sort in ascending order
+my_list.sort()
+
+# 7. Find and print the index of 30
+print(my_list.index(30))
